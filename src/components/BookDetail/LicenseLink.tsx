0613ac208cdcef85f7ba9c48e50832435bf6728f
John Hatton <hattonjohn@gmail.com>
2020-03-11 06:47:33 -0600

    LicenseLink: handle missing license

--- src/components/BookDetail/LicenseLink.tsx
+++ src/components/BookDetail/LicenseLink.tsx
@@ -3,19 +3,23 @@ import { Link } from "@material-ui/core";
 import React from "react";
 export const LicenseLink: React.FunctionComponent<{
     book: Book;
-}> = props => (
-    <Link
-        color="secondary"
-        target="_blank"
-        rel="noopener noreferrer"
-        href={
-            // enhance: can we point to the actual version?
-            `https://creativecommons.org/licenses/${props.book.license.replace(
-                "cc-",
-                ""
-            )}/4.0/`
-        }
-    >
-        {props.book.license}
-    </Link>
-);
+}> = props => {
+    return props.book.license ? (
+        <Link
+            color="secondary"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={
+                // enhance: can we point to the actual version?
+                `https://creativecommons.org/licenses/${props.book.license.replace(
+                    "cc-",
+                    ""
+                )}/4.0/`
+            }
+        >
+            {props.book.license}
+        </Link>
+    ) : (
+        <span>???</span>
+    );
+};
