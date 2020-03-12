90a913efd0feea094d383beca4a90d10b9de3b9f
andrew-polk <andrew_polk@sil.org>
2020-03-12 15:16:06 -0700

    Fix display of bookshelf names on Detail page

--- src/components/BookDetail/MetadataGroup.tsx
+++ src/components/BookDetail/MetadataGroup.tsx
@@ -4,7 +4,7 @@ import css from "@emotion/css/macro";
 import { jsx } from "@emotion/core";
 /** @jsx jsx */
 
-import React from "react";
+import React, { useContext } from "react";
 import { Book } from "../../model/Book";
 import { observer } from "mobx-react";
 import { LicenseLink } from "./LicenseLink";
@@ -13,95 +13,101 @@ import titleCase from "title-case";
 import { Link } from "@material-ui/core";
 import { commonUI } from "../../theme";
 import { BookAnalytics } from "./BookAnalytics";
+import { CachedTablesContext } from "../../App";
+import { getBookshelfDisplayName } from "../../model/Bookshelf";
+import { getTagDisplayName } from "../../model/Tag";
 
 export const MetadataGroup: React.FunctionComponent<{
     book: Book;
     breakToColumn: string;
-}> = observer(props => (
-    <div
-        id={"details"}
-        css={css`
-            display: flex;
-            max-width: calc(
-                100vw - ${commonUI.detailViewMargin} -
-                    ${commonUI.detailViewMargin}
-            );
-            @media (max-width: ${props.breakToColumn}) {
-                flex-direction: column-reverse;
-            }
-        `}
-    >
+}> = observer(props => {
+    const { bookshelves } = useContext(CachedTablesContext);
+
+    return (
         <div
-            id="column1"
+            id={"details"}
             css={css`
-                flex-grow: 1;
-            `}
-        >
-            {props.book.level && <div>{`Level ${props.book.level}`}</div>}
-            <div>{`${props.book.pageCount} Pages`}</div>
-            <div>{props.book.copyright}</div>
-            <div>
-                {"License: "}
-                <LicenseLink book={props.book} />
-            </div>
-            <div>
-                {"Uploaded "}
-                {`${props.book.uploadDate!.toLocaleDateString()} by ${obfuscateEmail(
-                    props.book.uploader
-                )}`}
-            </div>
-            <div>{`Last updated on ${props.book.updateDate!.toLocaleDateString()}`}</div>
-            {props.book.importedBookSourceUrl &&
-                props.book.importedBookSourceUrl.length > 0 && (
-                    <div>
-                        Imported from&nbsp;
-                        <Link
-                            color="secondary"
-                            href={props.book.importedBookSourceUrl}
-                        >
-                            {new URL(props.book.importedBookSourceUrl).host}
-                        </Link>
-                    </div>
-                )}
-            <BookAnalytics book={props.book} />
-        </div>
-        <div
-            id="column2"
-            // This would be more concise using min-width. But we use max-width above.
-            // If we mix the two, there is one pixel of width where they aren't consistent.
-            css={css`
-                min-width: 300px;
-                float: right;
-                margin-left: 40px;
+                display: flex;
+                max-width: calc(
+                    100vw - ${commonUI.detailViewMargin} -
+                        ${commonUI.detailViewMargin}
+                );
                 @media (max-width: ${props.breakToColumn}) {
-                    float: inherit;
-                    margin-left: 0;
+                    flex-direction: column-reverse;
                 }
             `}
         >
-            <div>
-                {"Features: "}
-                {props.book.features
-                    ? props.book.features
-                          .map(f => {
-                              return titleCase(f);
-                          })
-                          .join(", ")
-                    : []}
+            <div
+                id="column1"
+                css={css`
+                    flex-grow: 1;
+                `}
+            >
+                {props.book.level && <div>{`Level ${props.book.level}`}</div>}
+                <div>{`${props.book.pageCount} Pages`}</div>
+                <div>{props.book.copyright}</div>
+                <div>
+                    {"License: "}
+                    <LicenseLink book={props.book} />
+                </div>
+                <div>
+                    {"Uploaded "}
+                    {`${props.book.uploadDate!.toLocaleDateString()} by ${obfuscateEmail(
+                        props.book.uploader
+                    )}`}
+                </div>
+                <div>{`Last updated on ${props.book.updateDate!.toLocaleDateString()}`}</div>
+                {props.book.importedBookSourceUrl &&
+                    props.book.importedBookSourceUrl.length > 0 && (
+                        <div>
+                            Imported from&nbsp;
+                            <Link
+                                color="secondary"
+                                href={props.book.importedBookSourceUrl}
+                            >
+                                {new URL(props.book.importedBookSourceUrl).host}
+                            </Link>
+                        </div>
+                    )}
+                <BookAnalytics book={props.book} />
             </div>
-            <div>
-                {"Tags: "}
-                {props.book.tags
-                    .filter(t => !t.startsWith("system"))
-                    .map(t => {
-                        const parts = t.split(":");
-                        return parts[1];
-                    })
-                    .join(", ")}
+            <div
+                id="column2"
+                // This would be more concise using min-width. But we use max-width above.
+                // If we mix the two, there is one pixel of width where they aren't consistent.
+                css={css`
+                    min-width: 300px;
+                    float: right;
+                    margin-left: 40px;
+                    @media (max-width: ${props.breakToColumn}) {
+                        float: inherit;
+                        margin-left: 0;
+                    }
+                `}
+            >
+                <div>
+                    {"Features: "}
+                    {props.book.features
+                        ? props.book.features
+                              .map(f => {
+                                  return titleCase(f);
+                              })
+                              .join(", ")
+                        : []}
+                </div>
+                <div>
+                    {"Tags: "}
+                    {props.book.tags
+                        .filter(t => !t.startsWith("system"))
+                        .map(t => {
+                            return getTagDisplayName(t, bookshelves);
+                        })
+                        .join(", ")}
+                </div>
             </div>
         </div>
-    </div>
-));
+    );
+});
 function obfuscateEmail(user: any): string {
     const email = user.username;
     if (!email) {
