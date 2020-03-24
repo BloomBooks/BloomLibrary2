a7ce789b20642717aecc6a30b43746e1f2c85291
andrew-polk <andrew_polk@sil.org>
2020-03-24 12:14:12 -0700

    Show bookshelf display names rather than keys (BL-8262)

--- src/model/Tag.ts
+++ src/model/Tag.ts
@@ -1,19 +1,15 @@
-import { getBookshelfDisplayName } from "./Bookshelf";
-import { IBookshelfResult } from "../connection/LibraryQueryHooks";
-
 // Given a raw tag, return the simple text version we display to the user.
 // e.g.
 //   topic:Math => Math
-//   bookshelf:Parent/Child => Parent - Child
-export function getTagDisplayName(t: string, bookshelves: IBookshelfResult[]) {
+export function getTagDisplayName(t: string) {
     const parts = t.split(":");
-    const prefix = parts[0];
+    //const prefix = parts[0];
     const tagKey = parts[1];
-    if (prefix === "bookshelf") {
-        const bookshelf = bookshelves.find(shelf => shelf.key === tagKey);
-        if (bookshelf) {
-            return getBookshelfDisplayName(bookshelf.englishName);
-        }
-    }
+
+    // if (prefix === "bookshelf") {
+    // In Mar 2020, we copied bookshelf tags to the new bookshelves column,
+    // so they are handled elsewhere
+    // }
+
     return tagKey;
 }
