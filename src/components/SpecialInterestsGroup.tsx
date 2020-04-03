ddb4a70f27fff05f0c15c2376f9a8386c6341844
John Hatton <hattonjohn@gmail.com>
2020-04-03 09:15:08 -0600

    Icons for Special Interest Topics

--- src/components/SpecialInterestsGroup.tsx
+++ src/components/SpecialInterestsGroup.tsx
@@ -10,6 +10,10 @@ export const SpecialInterestGroup: React.FunctionComponent<IProps> = props => {
     return (
         <CategoryCardGroup {...props}>
             <CategoryCard
+                // TODO Andrew,
+                // 1) I've added svgs to assets for these
+                // 2) can we get these credits into the `title` attribute of the images
+                // credits Created by dDara from the Noun Project
                 key={"Health"}
                 title={"COVID-19 & Health Books"}
                 filter={{
@@ -19,6 +23,7 @@ export const SpecialInterestGroup: React.FunctionComponent<IProps> = props => {
                 img={""}
             />
             <CategoryCard
+                // credits Created by Andrew Doane from the Noun Project
                 key={"Bible"}
                 title={"Bible Books"}
                 filter={{
@@ -28,6 +33,7 @@ export const SpecialInterestGroup: React.FunctionComponent<IProps> = props => {
                 img={""}
             />
             <CategoryCard
+                // CC0 from the Noun Project
                 key={"STEM"}
                 title={"STEM Books"}
                 filter={{
