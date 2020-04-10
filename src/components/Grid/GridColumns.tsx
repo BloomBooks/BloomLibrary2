aa2b7e90225123b4306f9ab6593ae233b7c75595
John Hatton <hattonjohn@gmail.com>
2020-04-10 14:29:48 -0600

    Add phash to grid

--- src/components/Grid/GridColumns.tsx
+++ src/components/Grid/GridColumns.tsx
@@ -194,6 +194,7 @@ export function getBookGridColumnsDefinitions(): IGridColumn[] {
             },
         },
         { name: "pageCount", sortingEnabled: true },
+        { name: "phashOfFirstContentImage", sortingEnabled: true },
         { name: "createdAt", sortingEnabled: true },
         { name: "publisher", sortingEnabled: true },
         { name: "originalPublisher", sortingEnabled: true },
