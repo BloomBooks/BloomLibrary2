59536cbfa08b9bfe7f3865c3051d77022e9c589a
John Hatton <hattonjohn@gmail.com>
2020-04-21 09:08:44 -0600

    Leveled groups
    Introduces a

---
+++ src/components/LevelGroups.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BookGroup } from "./BookGroup";
+import { IFilter } from "../IFilter";
+import { getBestLevelStringOrEmpty } from "../connection/LibraryQueryHooks";
+
+// For each level (whether set by a human or just computed), show a row of books for that level.
+export const LevelGroups: React.FunctionComponent<{
+    filter: IFilter;
+}> = (props) => (
+    <React.Fragment>
+        {["1", "2", "3", "4"].map((level) => (
+            //  Enhance: this is going to show a book with a computedLevel even if it is over-ridden by `level`.
+
+            <BookGroup
+                title={"Level " + level}
+                filter={{ search: `level:${level}`, ...props.filter }}
+                secondaryFilter={(basicBookInfo) => {
+                    // At this point the query has given us books that have both level=level, and computedLevel=level
+                    // Here we drop those in which there is a level that is different than what we want, still keeping
+                    // books without a level in which there is still a computedLevel matching what we want.
+                    return getBestLevelStringOrEmpty(basicBookInfo) === level;
+                }}
+            />
+        ))}
+
+        {/* Show books that don't have a level */}
+        <BookGroup
+            title="Books for which we are missing levels"
+            rows={99}
+            filter={{ search: `level:empty`, ...props.filter }}
+        />
+    </React.Fragment>
+);
