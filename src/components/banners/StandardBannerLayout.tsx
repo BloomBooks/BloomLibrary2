33c1f448f241d14270b050904bd348079e96dc70
John Thomson <john_thomson@sil.org>
2020-06-23 17:44:41 -0500

    Make banner responsive
    logo can shrink, and if necessary move above text

--- src/components/banners/StandardBannerLayout.tsx
+++ src/components/banners/StandardBannerLayout.tsx
@@ -10,6 +10,7 @@ import { IFilter } from "../../IFilter";
 import { ICollection, IBanner } from "../../model/ContentInterfaces";
 import { ImgWithCredits } from "../../ImgWithCredits";
 import { Blurb } from "./Blurb";
+import { useMediaQuery } from "@material-ui/core";
 
 export const StandardBannerLayout: React.FunctionComponent<{
     collection: ICollection;
@@ -22,6 +23,9 @@ export const StandardBannerLayout: React.FunctionComponent<{
     const textColor = backgroundImage ? "white" : "black";
 
     const darkenBackgroundImageFraction = backgroundImage ? 0.4 : 0;
+
+    const showImageAndBlurbSideBySide = useMediaQuery(`(min-width:320px)`);
+    const direction = showImageAndBlurbSideBySide ? "row" : "column";
 
     return (
         <div
@@ -61,7 +65,7 @@ export const StandardBannerLayout: React.FunctionComponent<{
                 <div
                     css={css`
                         display: flex;
-                        flex-direction: row;
+                        flex-direction: ${direction};
                         max-height: 260px;
                         overflow: hidden;
                     `}
@@ -69,11 +73,20 @@ export const StandardBannerLayout: React.FunctionComponent<{
                     <LogoOnBanner
                         collection={props.collection}
                         banner={props.banner}
+                        // for wide images, height: auto makes them no higher
+                        // then they need to be for the available width.
+                        // For tall ones, half the usual height keeps them small
+                        // enough so some of the text fits.
+                        cssExtra={
+                            showImageAndBlurbSideBySide
+                                ? ""
+                                : "height: auto; max-height: 75px;"
+                        }
                     />
 
                     <Blurb
                         {...props}
-                        width={"100%"}
+                        width={"auto"}
                         hideTitle={
                             props.collection.hideLabelOnCardAndDefaultBanner ||
                             props.banner.hideTitle
@@ -94,6 +107,7 @@ export const StandardBannerLayout: React.FunctionComponent<{
 export const LogoOnBanner: React.FunctionComponent<{
     collection: ICollection;
     banner: IBanner;
+    cssExtra?: string;
 }> = (props) => {
     const logo = props.banner.logo
         ? props.banner.logo
@@ -108,9 +122,26 @@ export const LogoOnBanner: React.FunctionComponent<{
                         ? logo.altText
                         : "logo for " + props.banner.title
                 }
+                // complicated stuff is going on here with the display:flex on the
+                // parent div, particularly in row mode. FlexBox has an unobvious idea
+                // of the natural and minimum size of various things. By default an img
+                // with specified height can neight grow nor shrink, so as things get narrow,
+                // it shrinks the text as much as it can. Usually what it thinks is the minimum size
+                // is slightly more than the width of the Global Digital Library link (unwrapped).
+                // The space available with an unshrunk image can easily end up narrower
+                // than that, and then the text gets clipped
+                // (we're in overflow:hidden) both horizontally and vertically.
+                // Using object-fit:contain unobviously allows flex-box to shrink the image
+                // (without distorting it);
+                // object-position keeps it at the top left. Flex-box rather likes to shrink the
+                // margin-right; max-width:95% keeps SOME gap, though I haven't figured out exactly why.
                 css={css`
                     height: 150px;
                     margin-right: 50px;
+                    max-width: 95%;
+                    object-fit: contain;
+                    object-position: 0 0;
+                    ${props.cssExtra}
                 `}
             />
         )) || <React.Fragment></React.Fragment>
