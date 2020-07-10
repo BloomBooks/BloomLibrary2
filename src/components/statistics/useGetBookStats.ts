e1436269ee49dee6a2890e172f526e3e901832b0
Jeffrey Su <jeffrey_su@sil.org>
2020-07-10 13:48:30 -0500

    Change comprehension question stats to call new API endpoint

--- src/components/statistics/useGetBookStats.ts
+++ src/components/statistics/useGetBookStats.ts
@@ -20,10 +20,7 @@ export function useGetBookStats(props: IStatsProps): IBookStat[] {
 export function useGetBookComprehensionEventStats(
     props: IStatsProps
 ): IBookStat[] {
-    const { response } = useCollectionStats(
-        props,
-        "reading/per-book-comprehension"
-    );
+    const { response } = useCollectionStats(props, "reading/per-book");
 
     if (response && response["data"] && response["data"]["stats"])
         return response["data"]["stats"].map((s: any) => {
