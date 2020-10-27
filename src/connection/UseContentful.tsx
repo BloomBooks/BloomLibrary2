7bb09f16fdcad25e7db499293e7ea9971a88df09
andrew-polk <andrew_polk@sil.org>
2020-10-27 16:26:01 -0700

    Use localized Contentful content (BL-9072)

--- src/connection/UseContentful.tsx
+++ src/connection/UseContentful.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
+import { useIntl } from "react-intl";
 import { getContentfulClient } from "../ContentfulContext";
+
+const defaultContentfulLocale = "en-US";
 
 // Basically a map of queryString (created from query) to the raw Contentful query result.
 // This may be the entire set of collections or a single banner definition or any other
@@ -20,15 +23,22 @@ export function useContentful(
         /*unused*/ dummyToThrowStuffToErrorBoundaryInsideAHook,
     ] = useState();
 
+    const locale = useGetContentfulBestLocale() || defaultContentfulLocale;
+
     const queryString = JSON.stringify(query);
     useEffect(() => {
+        if (locale === "loading") return;
+
         if (contentfulCache[queryString]) {
-            setResults({ queryString, result: contentfulCache[queryString] });
+            setResults({
+                queryString,
+                result: contentfulCache[queryString],
+            });
             return;
         }
 
         getContentfulClient()
-            .getEntries({ include: 10, ...query })
+            .getEntries({ include: 10, locale, ...query })
             .then((entries: any) => {
                 contentfulCache[queryString] = entries.items;
                 setResults({ queryString, result: entries.items });
@@ -43,11 +53,54 @@ export function useContentful(
         // new http request just because the client's render creates
         // a new object with the same content on each call.
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [queryString]);
+    }, [queryString, locale]);
 
     if (!results || !results.result || results.queryString !== queryString) {
         return { loading: true, result: undefined };
     }
 
     return { loading: false, result: results.result };
+}
+
+function useGetContentfulBestLocale(): string | undefined {
+    const currentLocale = useIntl().locale;
+
+    const contentfulLocales = useGetContentfulLocales();
+    if (contentfulLocales.length) {
+        if (contentfulLocales.includes(currentLocale)) {
+            return currentLocale;
+        } else {
+            const iDash = currentLocale.indexOf("-");
+            if (iDash > 0) {
+                const localeToCheck = currentLocale.substring(0, iDash);
+                if (contentfulLocales.includes(localeToCheck))
+                    return localeToCheck;
+            }
+            return undefined;
+        }
+    }
+    return "loading";
+}
+
+let localesCache: string[] = [];
+function useGetContentfulLocales(): string[] {
+    // see https://github.com/facebook/react/issues/14981
+    const [
+        ,
+        /*unused*/ dummyToThrowStuffToErrorBoundaryInsideAHook,
+    ] = useState();
+
+    if (localesCache.length) return localesCache;
+
+    getContentfulClient()
+        .getLocales()
+        .then((results) => (localesCache = results.items.map((l) => l.code)))
+        .catch((err: Error) => {
+            console.error(JSON.stringify(err));
+            dummyToThrowStuffToErrorBoundaryInsideAHook(() => {
+                throw err;
+            });
+        });
+
+    return localesCache;
 }
