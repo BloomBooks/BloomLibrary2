696d4fdb18051166d4396b06b1b58e7ea2ec6355
Gordon Martin <gordon_martin@sil.org>
2020-11-03 14:50:52 -0800

    BL-9073 More About page changes
    * add colored bands alternating per section
    * make secondary markdown float to right
    * update several sections to be more like original
       About page
    * move all css out to "index.css" in prep for moving
       to component .less files someday

--- src/components/pages/ContentfulPage.tsx
+++ src/components/pages/ContentfulPage.tsx
@@ -1,8 +1,4 @@
-import css from "@emotion/css/macro";
 import React from "react"; // see https://github.com/emotion-js/emotion/issues/1156
-// these two lines make the css prop work on react elements
-import { jsx } from "@emotion/core";
-/** @jsx jsx */
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { ContentfulBasePage, useContentfulPage } from "./ContentfulBasePage";
 import { ContentfulMarkdownPart } from "../ContentfulMarkdownPart";
@@ -16,24 +12,12 @@ export const ContentfulPage: React.FunctionComponent<{ urlKey: string }> = (
     }
     const markdownContent = page.fields.markdownBody as string;
     const innards = (
-        <div
-            css={css`
-                max-width: 1000px;
-                margin-left: 30px;
-                margin-right: 30px;
-                h1 {
-                    font-size: 2rem;
-                }
-                img {
-                    width: 200px;
-                }
-            `}
-        >
+        <div className={`base-contentful-page contentful-page ${props.urlKey}`}>
             {/* Insert our custom components when the markdown has HTML that calls for them */}
             {/* Could not get this to compile <Markdown> {markdownContent} </Markdown> */}
             {/* {options:{overrides:{h1:{component:WindowsInstallerDownloads, props:{}}}}} */}
             {markdownContent
-                ? <ContentfulMarkdownPart markdown={markdownContent} />
+                ? <ContentfulMarkdownPart markdown={markdownContent}/>
                 : documentToReactComponents(page.fields.body)}
         </div>
     );
