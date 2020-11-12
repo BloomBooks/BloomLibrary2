b6b168b3e663b92381765c4145f6ee5f057b537a
John Hatton <hattonjohn@gmail.com>
2020-11-12 08:07:43 -0700

    Tweak how images show up in contentful pages

--- src/components/ContentfulMarkdownPart.tsx
+++ src/components/ContentfulMarkdownPart.tsx
@@ -1,3 +1,9 @@
+// this engages a babel macro that does cool emotion stuff (like source maps). See https://emotion.sh/docs/babel-macros
+import css from "@emotion/css/macro";
+// these two lines make the css prop work on react elements
+import { jsx } from "@emotion/core";
+/** @jsx jsx */
+
 import React from "react";
 import Markdown from "markdown-to-jsx";
 import { BloomReaderVersionNumber } from "./BloomReaderVersionNumber";
@@ -31,13 +37,24 @@ export const ContentfulMarkdownPart: React.FunctionComponent<{
 
     return (
         <div
-            className={`contentful-markdown-part ${props.column === Column.rightColumn ? "rightColumn" : ""}`}
+            css={css`
+                img {
+                    // prevent images in stories from being wider than the container
+                    max-width: 100%;
+                    // Conceivably these rules should only apply to stories? If they start to
+                    // mess up other kinds of pages, then we can deal with that.
+                    margin-top: 14px;
+                    margin-bottom: 14px;
+                    display: block; // makes the following centering rules work
+                    margin-left: auto;
+                    margin-right: auto;
+                }
+            `}
+            className={`contentful-markdown-part ${
+                props.column === Column.rightColumn ? "rightColumn" : ""
+            }`}
         >
-            <Markdown
-                options={options}
-            >
-                {props.markdown}
-            </Markdown>
+            <Markdown options={options}>{props.markdown}</Markdown>
         </div>
     );
 };
