e34f784718e55605c6723be4c33434d8114532d8
John Thomson <john_thomson@sil.org>
2021-01-05 10:31:06 -0600

    Make more components lazy
    (In the sense that, for example, the code only needed by detail view is made into a separate bundle and not fetched or parsed until needed)

---
+++ src/components/ReadBookPageLazy.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export interface IReadBookPageProps {
+    id: string;
+}
+
+// This is wrapped so that we can keep all the javascript involved in the BookDetail
+// in a separate js file, downloaded to the user's browser only if he/she needs it.
+export const ReadBookPageLazy: React.FunctionComponent<IReadBookPageProps> = (
+    props
+) => {
+    const ReadBookPage = React.lazy(
+        () => import(/* webpackChunkName: "readBookPage" */ "./ReadBookPage")
+    );
+    return (
+        <React.Suspense fallback={<div>Loading Book...</div>}>
+            <ReadBookPage {...props} />
+        </React.Suspense>
+    );
+};
--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -6,10 +6,10 @@ import { jsx } from "@emotion/core";
 
 import React, { useEffect } from "react";
 import { Switch, Route, Redirect, useLocation } from "react-router-dom";
-import { GridPage } from "./Grid/GridPage";
-import { BulkEditPage } from "./BulkEdit/BulkEditPage";
-import BookDetail from "./BookDetail/BookDetail";
-import { ReadBookPage } from "./ReadBookPage";
+import { GridPage } from "./Grid/GridPage"; // internally lazy
+import { BulkEditPageLazy } from "./BulkEdit/BulkEditPageLazy";
+import { BookDetailLazy } from "./BookDetail/BookDetailLazy";
+import { ReadBookPageLazy } from "./ReadBookPageLazy";
 import { CollectionSubsetPage } from "./CollectionSubsetPage";
 import { ContentfulBanner } from "./banners/ContentfulBanner";
 import { CollectionPage } from "./CollectionPage";
@@ -99,13 +99,13 @@ export const Routes: React.FunctionComponent<{}> = () => {
                 <Route
                     path="/:breadcrumbs*/book/:id"
                     render={({ match }) => {
-                        return <BookDetail id={match.params.id} />;
+                        return <BookDetailLazy id={match.params.id} />;
                     }}
                 />
                 <Route
                     path="/player/:id"
                     render={({ match }) => {
-                        return <ReadBookPage id={match.params.id} />;
+                        return <ReadBookPageLazy id={match.params.id} />;
                     }}
                 />
                 <Route
@@ -126,7 +126,9 @@ export const Routes: React.FunctionComponent<{}> = () => {
                 <Route
                     path="/bulk/:filter*"
                     render={({ match }) => {
-                        return <BulkEditPage filters={match.params.filter} />;
+                        return (
+                            <BulkEditPageLazy filters={match.params.filter} />
+                        );
                     }}
                 />
                 <Route
