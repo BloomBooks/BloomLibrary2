eb430ca6362dea7196e58afb885a5f2210e2f309
andrew-polk <andrew_polk@sil.org>
2021-01-08 16:05:44 -0700

    Prevent getLocales from being a performance bottleneck

--- src/connection/UseContentful.tsx
+++ src/connection/UseContentful.tsx
@@ -1,3 +1,4 @@
+import { EntryCollection } from "contentful";
 import { useState, useEffect } from "react";
 import { useIntl } from "react-intl";
 import { getContentfulClient } from "../ContentfulContext";
@@ -37,18 +38,34 @@ export function useContentful(
             return;
         }
 
-        getContentfulClient()
-            .getEntries({ include: 10, locale, ...query })
-            .then((entries: any) => {
-                contentfulCache[queryString] = entries.items;
-                setResults({ queryString, result: entries.items });
-            })
-            .catch((err: Error) => {
-                console.error(JSON.stringify(err));
-                dummyToThrowStuffToErrorBoundaryInsideAHook(() => {
-                    throw err;
+        getContentfulEntries(locale).catch(() => {
+            // This is just a safety net.
+            // Without this, if the hardcoded locale list in useGetContentfulLocales
+            // has a locale which isn't set up in Contentful, we would get an error.
+            getContentfulEntries(defaultContentfulLocale).catch(
+                (err: Error) => {
+                    console.error(JSON.stringify(err));
+                    dummyToThrowStuffToErrorBoundaryInsideAHook(() => {
+                        throw err;
+                    });
+                }
+            );
+        });
+
+        function getContentfulEntries(localeForEntries: string) {
+            return getContentfulClient()
+                .getEntries({
+                    include: 10, // depth
+                    locale: localeForEntries,
+                    ...query,
+                    limit: 1000, // without this, we would only get 100 collections
+                })
+                .then((entries: EntryCollection<unknown>) => {
+                    contentfulCache[queryString] = entries.items;
+                    setResults({ queryString, result: entries.items });
                 });
-            });
+        }
+
         // We want to depend on query, but not in a way that causes a
         // new http request just because the client's render creates
         // a new object with the same content on each call.
@@ -84,7 +101,14 @@ function useGetContentfulBestLocale(): string | undefined {
 
 let localesCache: string[] | undefined;
 function useGetContentfulLocales(): string[] | undefined {
+    return ["en-US", "es", "fr"];
+
+    // The code below here was used to dynamically get the list of locales from Contentful.
+    // But it was a 150-200ms bottleneck before the site loaded.
+    // I'm keeping the code in case we ever want to go back to it, since it was a bit tricky to get right.
+
     // see https://github.com/facebook/react/issues/14981
+    // eslint-disable-next-line no-unreachable
     const [
         ,
         /*unused*/ dummyToThrowStuffToErrorBoundaryInsideAHook,
@@ -96,9 +120,9 @@ function useGetContentfulLocales(): string[] | undefined {
 
     getContentfulClient()
         .getLocales()
-        .then((results) => {
-            //console.log(JSON.stringify(results, null, 4));
-            localesCache = results.items.map((l) => l.code);
+        .then((locales) => {
+            //console.log(JSON.stringify(locales, null, 4));
+            localesCache = locales.items.map((locale) => locale.code);
             // this is needed to wake up react somehow and cause things to re-render
             setLocalesCache(localesCache);
         })
@@ -113,5 +137,6 @@ function useGetContentfulLocales(): string[] | undefined {
     // this function will run again, and we will return the populated
     // localesCache. Until then, just return undefined which we treat as the
     // "loading" state.
+    // eslint-disable-next-line no-unreachable
     return tempLocalesCache;
 }
