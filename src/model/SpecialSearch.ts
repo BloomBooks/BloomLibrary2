7e74d7dc8ab45081ec9588450153bd33321576a4
Gordon Martin <gordon_martin@sil.org>
2021-04-06 08:50:11 -0700

    BL-9751 Keyword search phase 1
    * comment out unused Tooltip stuff
    * pull out all special searches to a separate file

---
+++ src/model/SpecialSearch.ts
@@ -0,0 +1,68 @@
+import { setLanguageOverride } from "../localization/LocalizationProvider";
+
+// We enter special key words in a Contentful Collection.
+// These key words will be used (after localization via Crowdin somehow) to tell blorg that it should
+// bring up a specific collection that is linked to those words.
+// The key words (and their "translations") should be minimal, since a user will no longer be able
+// search directly for books containing those words.
+
+// For now we are just making the hard-coded searches in SearchBox.tsx into something a bit more
+// generic.
+interface searchKeys {
+    keywords: string[];
+    url: string;
+}
+
+export const noPushCode: string = "xNoPushx";
+
+// The idea is to get this array from (translated) Contenful keywords field.
+const specialSearchArray: searchKeys[] = [
+    {
+        keywords: [
+            "covid",
+            "kovid",
+            "covid19",
+            "coronavirus",
+            "cov19",
+            "kovid19",
+        ],
+        url: "covid19",
+    },
+    {
+        keywords: ["kingstone", "comic bible", "bible comic", "bible comics"],
+        url: "super-bible",
+    },
+];
+
+export function trySpecialSearch(search: string): string[] {
+    // These 'magic' strings cause a file to be written without changing the state of the page.
+    if (search === "freelearningiocsv" || search === "csv") {
+        giveFreeLearningCsvAsync();
+        return [noPushCode];
+    }
+
+    if (search === "grid") {
+        return ["grid"]; // sends the user to the grid page.
+    }
+
+    // Allow developers/testers to switch the uilang by typing "uilang=fr". Only marginally useful
+    // because you lose it when you refresh. But it was going to be a pain to preserve it as
+    // a url parameter. Note that you can change your lang in browser settings pretty easily for a
+    // more permanent effect.
+    if (search.indexOf("uilang=") === 0) {
+        setLanguageOverride(search.split("=")[1]);
+        return [noPushCode];
+    }
+
+    const result = specialSearchArray.find((entry) =>
+        entry.keywords.includes(search)
+    );
+    return result ? [result.url] : [];
+}
+
+async function giveFreeLearningCsvAsync() {
+    const { giveFreeLearningCsv } = await import(
+        "../export/freeLearningIO" /* webpackChunkName: "freeLearningCsv" */
+    );
+    giveFreeLearningCsv();
+}
