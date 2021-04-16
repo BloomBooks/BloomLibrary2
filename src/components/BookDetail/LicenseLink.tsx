9bce75b8d9434e7748c113a4d7d4105a60b85908
John Thomson <john_thomson@sil.org>
2021-04-16 08:33:48 -0500

    Show licenseNotes when custom License clicked (BL-9713)

--- src/components/BookDetail/LicenseLink.tsx
+++ src/components/BookDetail/LicenseLink.tsx
@@ -1,9 +1,50 @@
+// this engages a babel macro that does cool emotion stuff (like source maps). See https://emotion.sh/docs/babel-macros
+import css from "@emotion/css/macro";
+// these two lines make the css prop work on react elements
+import { jsx } from "@emotion/core";
+/** @jsx jsx */
+
 import { Book } from "../../model/Book";
-import React from "react";
+import React, { useState } from "react";
 import { BlorgLink } from "../BlorgLink";
+import { Alert } from "../Alert";
+import { commonUI } from "../../theme";
 export const LicenseLink: React.FunctionComponent<{
     book: Book;
 }> = (props) => {
+    const [alertText, setAlertText] = useState("");
+    if (props.book.license === "custom") {
+        return (
+            <React.Fragment>
+                <Alert
+                    open={alertText !== ""}
+                    close={() => {
+                        setAlertText("");
+                    }}
+                    message={alertText!}
+                />
+                <button
+                    css={css`
+                        color: ${commonUI.colors.bloomBlue};
+                        background: none !important;
+                        border: none;
+                        padding: 0 !important;
+                        text-decoration: underline;
+                        cursor: pointer;
+                        font: inherit;
+                        &:focus {
+                            /* add outline to focus pseudo-class */
+                            outline-style: dotted;
+                            outline-width: 1px;
+                        }
+                    `}
+                    onClick={() => setAlertText(props.book.licenseNotes)}
+                >
+                    custom
+                </button>
+            </React.Fragment>
+        );
+    }
     return props.book.license ? (
         <BlorgLink
             color="secondary"
