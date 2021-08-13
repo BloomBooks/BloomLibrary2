a231054c7eea1e175cc2eb9d774ed697d12318f8
Stephen McConnel <stephen_mcconnel@sil.org>
2021-08-13 09:34:53 -0600

    Get total books counts for parent collections (BL-10187)

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -23,6 +23,7 @@ import { useResponsiveChoice } from "../responsiveUtilities";
 import { ICardSpec, useBaseCardSpec } from "./RowOfCards";
 import { commonUI } from "../theme";
 import TruncateMarkup from "react-truncate-markup";
+import { getFilterForCollectionAndChildren } from "../model/Collections";
 
 export enum CollectionCardLayout {
     short,
@@ -260,8 +261,17 @@ export const CollectionCard: React.FunctionComponent<{
                     font-size: ${getResponsiveChoice(10, 14)}px;
                 `}
             >
-                {props.collection.filter && (
-                    <BookCount filter={props.collection.filter} />
+                {(props.collection.filter ||
+                    props.collection.childCollections) && (
+                    <BookCount
+                        filter={
+                            props.collection.filter
+                                ? props.collection.filter
+                                : getFilterForCollectionAndChildren(
+                                      props.collection
+                                  )
+                        }
+                    />
                 )}
             </div>
         </CheapCard>
