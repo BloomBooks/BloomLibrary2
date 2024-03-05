6c6d3d44b911135c71a72363fb4166a854abd1fa
Andrew Polk <andrew_polk@sil.org>
2024-03-05 14:12:18 -0700

    Allow collection editors to delete books (BL-12726)

--- src/components/BookDetail/BookOwnerControlsBox.tsx
+++ src/components/BookDetail/BookOwnerControlsBox.tsx
@@ -20,11 +20,18 @@ import { AvatarCircle, LoggedInFirebaseUser } from "../User/AvatarCircle";
 import { User, useGetUserIsModerator } from "../../connection/LoggedInUser";
 import { getCurrentUser } from "../../authentication/firebase/firebase";
 import firebase from "firebase/compat/app";
+import {
+    IUserBookPermissions,
+    useGetPermissions,
+} from "../../connection/LibraryQueryHooks";
 
 // This should become true or just be removed once 5.7 is shipping.
 // The controls it hides require 5.7, so we don't want ordinary users to see them until then.
-// We do want to be able to test this on our dev site, though.
-const bloom57IsShipping = window.location.hostname.startsWith("dev");
+// We do want to be able to test this on our dev and alpha sites, though.
+const allowDownloadForEditing =
+    window.location.hostname.startsWith("alpha") ||
+    window.location.hostname.startsWith("dev") ||
+    window.location.hostname.startsWith("localhost");
 
 export const BookOwnerControlsBox: React.FunctionComponent<{
     book: Book;
@@ -42,177 +49,209 @@ export const BookOwnerControlsBox: React.FunctionComponent<{
             }
         });
     }, [props.user]);
+
     const userIsUploader =
         props.user.username === props.book.uploader?.username;
     const userIsModerator = useGetUserIsModerator();
-    if (userIsModerator || userIsUploader) {
-        return (
-            <div
-                css={css`
-                    box-sizing: border-box;
-                    padding: 1em;
-                    margin-top: 20px;
-                    margin-bottom: 20px;
-                    border: 4px solid ${commonUI.colors.bloomBlue};
-                    border-radius: 5px;
-                `}
-            >
-                <div>
-                    <div
+    const permissions: IUserBookPermissions = useGetPermissions(
+        !!firebaseUser,
+        props.book.id
+    );
+
+    const showControlsBox =
+        // We expect userIsModerator to become obsolete as soon as we have Users set up in Contentful.
+        // At that point, moderators will have all permissions.x as true.
+        userIsModerator ||
+        // See comment below about userIsUploader.
+        userIsUploader ||
+        permissions.editSurfaceMetadata === true ||
+        permissions.reupload === true ||
+        permissions.delete === true;
+
+    if (!showControlsBox) return <Fragment />;
+
+    const showSurfaceMetadataControls =
+        userIsModerator ||
+        // For now, we use userIsUploader because we have not yet implemented book modification
+        // for non-uploaders. When we do, this should use editSurfaceMetadata instead.
+        userIsUploader;
+    //permissions.editSurfaceMetadata === true;
+    const showDownloadForEditButton =
+        allowDownloadForEditing && permissions.reupload === true;
+    const showDeleteButton = userIsModerator || permissions.delete === true;
+
+    return (
+        <div
+            css={css`
+                box-sizing: border-box;
+                padding: 1em;
+                margin-top: 20px;
+                margin-bottom: 20px;
+                border: 4px solid ${commonUI.colors.bloomBlue};
+                border-radius: 5px;
+            `}
+        >
+            <div>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    <AvatarCircle
+                        buttonHeight="28px"
+                        loggedInUser={firebaseUser as LoggedInFirebaseUser}
+                    ></AvatarCircle>
+                    <h1
+                        id={"book.detail.youHavePermission"}
                         css={css`
-                            display: flex;
-                            align-items: center;
-                        `}
-                    >
-                        <AvatarCircle
-                            buttonHeight="28px"
-                            loggedInUser={firebaseUser as LoggedInFirebaseUser}
-                        ></AvatarCircle>
-                        <h1
-                            id={"book.detail.youHavePermission"}
-                            css={css`
-                                color: ${commonUI.colors.bloomBlue};
-                                margin-top: 0;
-                                margin-bottom: 0;
-                                display: inline-block;
-                                margin-left: 10px;
-                            `}
-                        >
-                            You have permission to modify this book
-                        </h1>
-                    </div>
-                    <h2
-                        css={css`
-                            margin-bottom: 0;
                             color: ${commonUI.colors.bloomBlue};
-                        `}
-                        id="book.detail.draft"
-                    >
-                        Draft
-                    </h2>
-                    <FormControlLabel
-                        css={css`
-                            margin-top: 5px;
-                        `}
-                        control={
-                            <Checkbox
-                                css={css`
-                                    padding-top: 0;
-                                    margin-right: -5px;
-                                    padding-right: 1px;
-                                `}
-                                checked={props.book.draft}
-                                onChange={(e) => {
-                                    props.book.draft = e.target.checked;
-                                    props.book.saveAdminDataToParse();
-                                }}
-                            />
-                        }
-                        label={
-                            <div
-                                css={css`
-                                    display: flex;
-                                `}
-                            >
-                                <DraftIcon
-                                    css={css`
-                                        width: 54px;
-                                    `}
-                                />
-                                <div>
-                                    {l10n.formatMessage({
-                                        id: "book.detail.draftDescription",
-                                        defaultMessage:
-                                            "Do not show this book to the public yet. I will share its URL with reviewers for feedback.",
-                                        description:
-                                            "Label for a check box which, if checked, marks the book as 'DRAFT' and prevents the book from showing in most views",
-                                    })}
-                                </div>
-                            </div>
-                        }
-                    />
-                    <div
-                        css={css`
-                            display: flex;
-                            flex-direction: column;
+                            margin-top: 0;
+                            margin-bottom: 0;
+                            display: inline-block;
+                            margin-left: 10px;
                         `}
                     >
+                        You have permission to modify this book
+                    </h1>
+                </div>
+                {showSurfaceMetadataControls && (
+                    <>
                         <h2
                             css={css`
                                 margin-bottom: 0;
                                 color: ${commonUI.colors.bloomBlue};
                             `}
-                            id="book.detail.editing"
+                            id="book.detail.draft"
                         >
-                            Editing
+                            Draft
                         </h2>
-                        <Alert severity="info">
-                            <div>
-                                <FormattedMessage
-                                    id={"book.detail.updateBookNotice"}
-                                    defaultMessage={
-                                        "If you want to update this book with any changes, just upload it again from Bloom, using the same account. Your new version will replace this one."
-                                    }
+                        <FormControlLabel
+                            css={css`
+                                margin-top: 5px;
+                            `}
+                            control={
+                                <Checkbox
+                                    css={css`
+                                        padding-top: 0;
+                                        margin-right: -5px;
+                                        padding-right: 1px;
+                                    `}
+                                    checked={props.book.draft}
+                                    onChange={(e) => {
+                                        props.book.draft = e.target.checked;
+                                        props.book.saveAdminDataToParse();
+                                    }}
                                 />
-                            </div>
-                            {bloom57IsShipping && (
+                            }
+                            label={
                                 <div
                                     css={css`
-                                        margin-top: 10px;
+                                        display: flex;
                                     `}
                                 >
-                                    <FormattedMessage
-                                        id={"book.detail.getForEditBookNotice"}
-                                        defaultMessage={
-                                            "If necessary, we can give you the book to edit in Bloom. You must first have Bloom 5.7 or greater installed ({downloadLink})."
-                                        }
-                                        values={{
-                                            downloadLink: (
-                                                <BlorgLink
-                                                    href="page/create/downloads"
-                                                    css={css`
-                                                        color: ${commonUI.colors
-                                                            .bloomBlue};
-                                                    `}
-                                                >
-                                                    <FormattedMessage
-                                                        id={
-                                                            "book.detail.downloadBloom"
-                                                        }
-                                                        defaultMessage={
-                                                            "Download Bloom"
-                                                        }
-                                                    />
-                                                </BlorgLink>
-                                            ),
-                                        }}
+                                    <DraftIcon
+                                        css={css`
+                                            width: 54px;
+                                        `}
                                     />
+                                    <div>
+                                        {l10n.formatMessage({
+                                            id: "book.detail.draftDescription",
+                                            defaultMessage:
+                                                "Do not show this book to the public yet. I will share its URL with reviewers for feedback.",
+                                            description:
+                                                "Label for a check box which, if checked, marks the book as 'DRAFT' and prevents the book from showing in most views",
+                                        })}
+                                    </div>
                                 </div>
-                            )}
-                        </Alert>
-                        {bloom57IsShipping && (
-                            <Button
-                                onClick={() =>
-                                    props.showDownloadDialog.current?.()
+                            }
+                        />
+                    </>
+                )}
+                <div
+                    css={css`
+                        display: flex;
+                        flex-direction: column;
+                    `}
+                >
+                    <h2
+                        css={css`
+                            margin-bottom: 0;
+                            color: ${commonUI.colors.bloomBlue};
+                        `}
+                        id="book.detail.editing"
+                    >
+                        Editing
+                    </h2>
+                    <Alert severity="info">
+                        <div>
+                            <FormattedMessage
+                                id={"book.detail.updateBookNotice"}
+                                defaultMessage={
+                                    "If you want to update this book with any changes, just upload it again from Bloom, using the same account. Your new version will replace this one."
                                 }
-                                color="secondary"
-                                variant="outlined"
+                            />
+                        </div>
+                        {showDownloadForEditButton && (
+                            <div
                                 css={css`
-                                    align-self: flex-end;
-                                    margin-top: 5px;
+                                    margin-top: 10px;
                                 `}
                             >
                                 <FormattedMessage
-                                    id={"book.detail.editDownload"}
+                                    id={"book.detail.getForEditBookNotice"}
                                     defaultMessage={
-                                        "Download into Bloom for editing"
+                                        "If necessary, we can give you the book to edit in Bloom. You must first have Bloom 5.7 or greater installed ({downloadLink})."
                                     }
+                                    values={{
+                                        downloadLink: (
+                                            <BlorgLink
+                                                href="page/create/downloads"
+                                                css={css`
+                                                    color: ${commonUI.colors
+                                                        .bloomBlue};
+                                                `}
+                                            >
+                                                <FormattedMessage
+                                                    id={
+                                                        "book.detail.downloadLink"
+                                                    }
+                                                    defaultMessage={
+                                                        "Download Bloom"
+                                                    }
+                                                />
+                                            </BlorgLink>
+                                        ),
+                                    }}
                                 />
-                            </Button>
+                            </div>
                         )}
-                    </div>
+                    </Alert>
+                    {showDownloadForEditButton && (
+                        <Button
+                            onClick={() => props.showDownloadDialog.current?.()}
+                            color="secondary"
+                            variant="outlined"
+                            css={css`
+                                align-self: flex-end;
+                                margin-top: 5px;
+                            `}
+                        >
+                            <FormattedMessage
+                                id={"book.detail.editDownload"}
+                                defaultMessage={
+                                    "Download into Bloom for editing"
+                                }
+                            />
+                        </Button>
+                    )}
                 </div>
+            </div>
+            {showSurfaceMetadataControls && (
                 <BookExtraPanels book={props.book} />
+            )}
+            {showDeleteButton && (
                 <div
                     css={css`
                         margin-top: 30px;
@@ -220,8 +259,7 @@ export const BookOwnerControlsBox: React.FunctionComponent<{
                 >
                     <DeleteButton book={props.book} />
                 </div>
-            </div>
-        );
-    }
-    return <Fragment />;
+            )}
+        </div>
+    );
 });
--- src/connection/LibraryUpdates.ts
+++ src/connection/LibraryUpdates.ts
@@ -1,23 +1,12 @@
 import { axios } from "@use-hooks/axios";
 import { getConnection } from "./ParseServerConnection";
 
-export function updateBook(
-    bookId: string,
-    params: object,
-    currentSession?: string
-): void {
+export function updateBook(bookId: string, params: object): void {
     if (!bookId || !params) {
         return;
     }
 
     const headers = getConnection().headers;
-    // currentSession is for old BloomLibrary code. In BL2, the login
-    // process includes putting a session token into the headers that getConnection() returns.
-    if (currentSession) {
-        Object.assign(headers, {
-            "X-Parse-Session-Token": currentSession,
-        });
-    }
 
     // Without this, the code assumes the update comes from an upload from BloomDesktop
     // and certain unwanted changes would be made to the book record
