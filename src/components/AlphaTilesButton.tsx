de8581abba8402f80342e3fda9f49c29fb1a6bb5
Hatton <hattonjohn@gmail.com>
2024-07-23 19:59:25 -0600

    Add Get Alpha Tiles Buttons to Collections

---
+++ src/components/AlphaTilesButton.tsx
@@ -0,0 +1,91 @@
+// this engages a babel macro that does cool emotion stuff (like source maps). See https://emotion.sh/docs/babel-macros
+import css from "@emotion/css/macro";
+// these two lines make the css prop work on react elements
+import { jsx } from "@emotion/core";
+/** @jsx jsx */
+
+import React from "react";
+import AlphaTilesLogo from "../assets/AlphaTilesMinimal.png";
+import { CheapCard } from "./CheapCard";
+import GooglePlayLogo_EN from "../assets/GetItOnGooglePlay_en.png";
+import GooglePlayLogo_ES from "../assets/GetItOnGooglePlay_es.png";
+import GooglePlayLogo_FR from "../assets/GetItOnGooglePlay_fr.png";
+import { useIntl } from "react-intl";
+
+export const AlphaTilesButton: React.FunctionComponent<{
+    languageName: string;
+    url: string;
+}> = (props) => {
+    const l10 = useIntl();
+    const description = l10.formatMessage(
+        {
+            id: "alphaTilesDescription",
+            defaultMessage: "{languageName} Literacy Game App",
+        },
+        { languageName: props.languageName }
+    );
+    const language = navigator.language.toLowerCase();
+    let googlePlayLogo = GooglePlayLogo_EN;
+
+    if (language.includes("es")) {
+        googlePlayLogo = GooglePlayLogo_ES;
+    } else if (language.includes("fr")) {
+        googlePlayLogo = GooglePlayLogo_FR;
+    }
+
+    return (
+        <CheapCard
+            css={css`
+                flex-direction: row;
+                width: fit-content;
+                padding: 10px;
+                margin-left: auto;
+            `}
+            url={props.url}
+        >
+            <div
+                css={css`
+                    display: flex;
+                    flex-direction: row;
+                `}
+            >
+                <img alt="Alpha Tiles Logo" src={AlphaTilesLogo} />
+                <div
+                    css={css`
+                        margin-top: auto;
+                        margin-bottom: auto;
+                        margin-left: 5px;
+                        margin-right: 20px;
+                    `}
+                >
+                    <div
+                        css={css`
+                            font-weight: bold;
+                            font-size: 14px;
+                            line-height: 1em;
+                        `}
+                    >
+                        {description}
+                    </div>
+                    <div
+                        css={css`
+                            font-weight: bold;
+                            font-size: 12px;
+                        `}
+                    >
+                        {/* as far as I can tell, they don't localize this */}
+                        Alpha Tiles
+                    </div>
+                </div>
+            </div>
+            <img
+                css={css`
+                    height: 45px;
+                    width: auto;
+                `}
+                alt="Google Play Logo"
+                src={googlePlayLogo}
+            />
+        </CheapCard>
+    );
+};
--- src/model/ContentInterfaces.ts
+++ src/model/ContentInterfaces.ts
@@ -51,6 +51,7 @@ export interface ICollection {
     description: string;
     urlForBloomPubBundle: string; // used where someone (i.e. Education for Life) is providing a link to a bloomBundle of all the books in the collection.
     urlForBloomSourceBundle: string; // used for collections of shells where it is common for people to want to translate the whole set
+    urlForAlphaTiles: string;
     filter?: IFilter; // contentful does give us undefined if this field is empty
     statisticsQuerySpec?: IStatisticsQuerySpec;
     iconForCardAndDefaultBanner?: IMedia;
